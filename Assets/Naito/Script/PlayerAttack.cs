b4da3e5123ee2069b10775b9125b95ba66e337a5
naitoo0205 <100120605+naitoo0205@users.noreply.github.com>
2022-10-05 10:51:31 +0900

    PulsEnemy,Bullet

---
+++ Assets/Naito/Script/PlayerAttack.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("AttackAreaDereta", 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void AttackAreaDereta()
+    {
+        Destroy(this.gameObject);
+    }
+}
